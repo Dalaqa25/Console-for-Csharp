bfda98df8c14247f11fa1f78be58e90896531631
dalaqa25 <g.dalaqishvili01@gmail.com>
2024-07-22 11:47:54 +0400

    FirstOrDefault

---
+++ FirstOrDefault/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+
+public class SecondTry
+{
+    public int Id {get; set;}
+    public string Name {get; set;} = string.Empty;
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var stack = new List<SecondTry>
+        {
+            new SecondTry {Id = 2, Name = "giorgio"},
+            new SecondTry {Id = 3, Name = "gela"}
+        };
+        int IdtoIndex = 3;
+        var Result = stack.FirstOrDefault(x => x.Id == IdtoIndex);
+        
+        if (Result != null)
+        {
+            Console.WriteLine($"Found: {Result.Name}");
+        }
+        else 
+        {
+           Console.WriteLine("nop!"); 
+        }
+    }
+}
